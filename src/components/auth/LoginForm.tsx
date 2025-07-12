589fb5376499f420f1f6d492d8bf72f085e54fa5
gentleyonster <titusau3@gmail.com>
2025-07-12 20:21:16 +0800

    Fix LoginForm Syntax Error

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -167,7 +167,6 @@ export function LoginForm({ onSuccess, onSwitchToSignup }: LoginFormProps) {
 
         <div className="mt-6 text-center">
           <p className="text-gray-600">
-            Don't have an account?{' '}
             <button
               onClick={onSwitchToSignup}
               className="text-blue-600 hover:text-blue-700 font-medium"
