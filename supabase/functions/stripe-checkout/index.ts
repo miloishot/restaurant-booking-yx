eb69a3e67f2bc2cfd7186d3715fcafc4d96f9523
kahweng <89070480+miloishot@users.noreply.github.com>
2025-07-16 19:39:10 +0800

    Updated index.ts

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -188,7 +188,7 @@ Deno.serve(async (req) => {
       // Create checkout session for restaurant order
       const session = await stripe.checkout.sessions.create({
         customer: newCustomer.id,
-        payment_method_types: ['card','paynow'],
+        payment_method_types: ['card'],
         line_items,
         mode: 'payment',
         automatic_tax: {
